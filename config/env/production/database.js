e17813d72c6fdfc0dfa3e93beffe28bf6a9154f5
abhijith <abhijith@pixelflames.com>
2023-08-17 17:15:03 +0530

    new updates

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,32 +1,32 @@
-    // // strapi-api/config/database.js
-    // module.exports = ({ env }) => ({
-    //   connection: {
-    //     client: 'postgres',
-    //     connection: {
-    //       host: env('DATABASE_HOST'),
-    //       port: env.int('DATABASE_PORT'),
-    //       database: env('DATABASE_NAME'),
-    //       user: env('DATABASE_USERNAME'),
-    //       password: env('DATABASE_PASSWORD'),
-    //       schema: env('DATABASE_SCHEMA'), // Not required
-    //       ssl: {
-    //         rejectUnauthorized: env.bool('DATABASE_SSL_SELF'), // For self-signed certificates
-    //       },
-    //     },
-    //     debug: false,
-    //   },
-    // });
+// // strapi-api/config/database.js
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST'),
+//       port: env.int('DATABASE_PORT'),
+//       database: env('DATABASE_NAME'),
+//       user: env('DATABASE_USERNAME'),
+//       password: env('DATABASE_PASSWORD'),
+//       schema: env('DATABASE_SCHEMA'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF'), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
 
-    // path: ./config/env/production/database.js
+// path: ./config/env/production/database.js
 
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  
+
   return {
     connection: {
-      client: 'postgres',
+      client: "postgres",
       connection: {
         host,
         port,
@@ -37,5 +37,5 @@ module.exports = ({ env }) => {
       },
       debug: false,
     },
-  }
+  };
 };
